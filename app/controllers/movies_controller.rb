034263491dbaf8885f0d6a2efb245e073d25539e
Shady ElDawy <shadyeldawy96@gmail.com>
2024-06-15 00:42:17 +0300

    view all movie data

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,7 @@ class MoviesController < ApplicationController
   def set_movies
     cache_key = generate_cache_key('movies', params.slice(:actor))
     @movies ||= Rails.cache.fetch(cache_key) do
+      ##we can select only what we need to further optimize, we can also use pagination
       Movie.sort_by_average_stars.by_actor(params[:actor])
     end
   end
